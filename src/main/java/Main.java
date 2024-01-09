9fc26d3dc1b24f92317a1200fc1a4590e75567a9
Akash <jadhav.aksh1999@gmail.com>
2024-01-09 11:50:38 +0530

    Created Welcome View

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
+import views.Welcome;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Welcome w= new Welcome();
+        do {
+            w.welcomeScreen();
+        }while(true);
     }
 }
